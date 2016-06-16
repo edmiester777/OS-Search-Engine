8161f9eccfc36aecf5e6e2bf96f679cd69561221
Intricate <intricate@users.noreply.github.com>
2016-06-16 18:31:33 -0400

    Added the NetScanner directory (isn't added to the project as yet)
    - This multiprocessing system scans IPv4 ranges and retrieves PTR records

---
+++ NetScanner/NetScanner.py
@@ -0,0 +1,29 @@
+﻿import socket
+from ipaddress import IPv4Address
+
+class NetScanner:
+    """This class scans a given range of IPv4 addresses for PTR records and probes specified services."""
+
+    def __init__(self, scannerId, probeServices = True):
+        self.scannerId = scannerId
+        self.probeServices = probeServices
+
+    def scanRange(self, startIp, endIp):
+        for i in range(startIp, endIp):
+            try:
+                print("[SCNR #{:02}] ".format(self.scannerId), end="")
+                ipv4Address = IPv4Address(i).exploded
+                hostnameInfo = socket.getnameinfo((ipv4Address, 0), socket.NI_NAMEREQD)
+                print("PTR record: {}".format(hostnameInfo))
+                if self.probeServices:
+                    self.probeService(ipv4Address, 'http')
+            except socket.error as e:
+                # error handling at its finest...
+                print(e.args)
+                pass    
+
+    def probeService(self, ip, service):
+        pass
+
+    def insertRecordIntoDb(hostname, ip):
+        pass
---
+++ NetScanner/NetScannerExecutor.py
@@ -0,0 +1,28 @@
+﻿from concurrent.futures import ProcessPoolExecutor
+from NetScanner.NetScanner import NetScanner
+
+class NetScannerExecutor(ProcessPoolExecutor):
+    """A child class of ProcessPoolExecutor which executes a specified number of NetScanners"""
+
+    CLASS_A_BEGIN = 0x01000000
+    CLASS_A_END = 0x7FFFFFFF
+    CLASS_B_BEGIN = 0x80000000
+    CLASS_B_END = 0xBFFFFFFF
+    CLASS_C_BEGIN = 0xC0000000
+    CLASS_C_END = 0xDFFFFFFF
+
+    def __init__(self, max_workers = None):
+        return super().__init__(max_workers)
+
+    def execute(self):
+        startIp = self.CLASS_A_BEGIN
+        endIp = self.CLASS_A_END
+        numIpsPerWorker = int((endIp - startIp) / self._max_workers)
+        for i in range(self._max_workers):
+            scanner = NetScanner(i, probeServices = True)
+            self.submit(scanner.scanRange, startIp, (startIp + numIpsPerWorker))
+            startIp = startIp + numIpsPerWorker
+        self.shutdown(wait = True)
+
+
+
